e8f2c33d7430f5d39944e39fbd7e747bcf6a3703
Martin Kloeppner <martin.kloeppner@innogames.de>
2015-04-14 01:51:03 +0200

    Wrap up commands

--- src/main/java/com/mkloeppner/ExtCommands.java
+++ src/main/java/com/mkloeppner/ExtCommands.java
@@ -1,25 +1,36 @@
 package com.mkloeppner;
 
 import com.mkloeppner.core.commands.ExtCommand;
+import com.mkloeppner.core.commands.ExtCommandDelegate;
 import com.mkloeppner.core.commands.ExtCommandParam;
 import com.mkloeppner.core.commands.ExtGroupCommand;
 import com.mkloeppner.core.commands.bungee.ExtBungeeCommand;
+import com.mkloeppner.core.commands.bungee.ExtBungeeCommandTextComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class ExtCommands extends Plugin {
+import java.util.List;
+
+public class ExtCommands extends Plugin implements ExtCommandDelegate {
 
     @Override
     public void onLoad() {
         super.onLoad();
 
-        ExtGroupCommand clanCmd = new ExtGroupCommand("clan");
+        ExtGroupCommand clanCmd = new ExtGroupCommand("ext");
 
-        ExtCommand inviteCmd = new ExtCommand("invite", null);
+        ExtCommand inviteCmd = new ExtCommand("cmds", this);
         inviteCmd.addParameter(new ExtCommandParam("player"));
+        clanCmd.addSubcommand(inviteCmd);
 
-        ExtBungeeCommand asBungeeCommand = new ExtBungeeCommand(inviteCmd);
+        ExtBungeeCommand asBungeeCommand = new ExtBungeeCommand(clanCmd);
 
         getProxy().getPluginManager().registerCommand(this, asBungeeCommand);
 
+    }
+
+    @Override
+    public void executeCommand(ExtCommand command, List<ExtCommandParam> parameters) {
+        command.getCommandSender().sendMessage(new ExtBungeeCommandTextComponent(new TextComponent("test")));
     }
 }
